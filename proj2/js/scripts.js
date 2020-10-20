db6ab1427c924232b7390de15e02e450cc294a81
MikeBrooksNH <3639849+MikeBrooksNH@users.noreply.github.com>
2020-10-20 18:36:31 -0400

    adding the course project as a sub web

---
+++ proj2/js/scripts.js
@@ -0,0 +1,10 @@
+/* JavaScript document */
+function lastupdated() {
+    var x = document.lastModified;
+    document.getElementById("lastupdated").innerHTML = x;
+}
+
+function unhide(elemid) {
+    document.getElementById(elemind).visibility = "visible";
+    console.write("elemid");
+}
